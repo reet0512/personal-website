1848965ecec79f6158b94313c49b99f772fe8cfa
reet0512 <reetkothari0512@gmail.com>
2023-07-03 20:20:18 -0700

    Small improvements

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <section id="home-page">
             <div>
-                <h1>Tech Enthusiast, Determined Developer, & Competitive Coder</h1>
+                <h1>Metalhead, Tech Enthusiast, Competitive Coder</h1>
             </div>
         </section>
     );
