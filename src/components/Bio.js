041facc0d65c983b1fa8ce5230db827599b1be36
Reet Kothari <reetkothari0512@gmail.com>
2025-02-08 23:11:10 -0500

    Updated porfolio with new roles and resume

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,15 +7,15 @@ export default function Bio() {
             <div id="bio-content">
                 <div id = "bio-para">
                     <p>
-                        Hello! I'm Reet Kothari and I'm originally from Bangalore, India. I am currently pursuing a Masters degree in Computer Science at Dartmouth College. 
-                        I have a strong interest in Data Science, Full-Stack Development, Data Analytics, and Distributed Architecture. 
+                        Hello! I'm Reet Kothari and I'm originally from Bangalore, India. I'm currently pursuing a Masters degree in Computer Science at Dartmouth College. 
+                        I have a strong interest in Data Science, Quantitative Strategies, and Full-Stack Development. 
                         I am fluent in English, Hindi, and Marwari. During my free time, I enjoy cooking, playing Squash, Chess, Video Games, and Guitar. 
                         Things I love:
                     </p>
                     <div id="icon-info">
                         <i className="fa-solid fa-headphones fa-xl icons"></i> {' '} Genre: Rock Music
                         <br/>
-                        <i className="fa-solid fa-chess-pawn fa-xl icons"></i> {' '} Chess.com Blitz: 1508
+                        <i className="fa-solid fa-chess-pawn fa-xl icons"></i> {' '} Chess.com Blitz: 1557
                         <br/>
                         <i className="fa-solid fa-gamepad fa-xl icons"></i> {' '} Video Games: Rocket League, Fortnite, Valorant
                         <br/>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,22 +1,26 @@
 import "./styles/experience.css";
-// import PerasiaLogo from "../docs/perasia_logo.png";
-// import SiemensLogo from "../docs/siemens_logo.png";
 import { useState } from "react";
 import {Modal, ModalHeader, ModalBody} from "reactstrap";
 
 
 const companyDesc = {
-    "Dozee": {
+    "Dartmouth College": {
+        "startDate": "October 07, 2024",
+        "endDate": "Present",
+        "location": "Hanover, NH",
+        "desc": "At Dartmouth, I worked as a Research Assistant and Teaching Assistant. For my research, I worked under Prof. Sarah Preum on the NLP task of Health Conflict Detection. As a TA, I developed rubrics, graded assignments, hosted office hours, and helped setup events for the machine learning course."
+    },
+    "Dozee Health": {
         "startDate": "April 01, 2024",
         "endDate": "August 09, 2024",
-        "location": "Bangalore, KA, India (In-person)",
+        "location": "Bangalore, KA, India",
         "desc": "During my time at Dozee as a Data Scientist, I developed an algorithm to annotate apnea events from ballistocardiogram data and used metrics like F1, precision, and Cohen's Kappa to evaluatethe it. In the process, I also implemented an internal tool to syncronize the JJ and RR peaks so that the lag between the BCG and ECG signals can be rectified to make working with new data easier. This automated tons of manual labor and sped up the development process by atleast a week."
     },
     "Perasia Technologies LLC": {
         "startDate": "April 04, 2023",
         "endDate": "August 25, 2023",
         "location": "Okemos, MI, USA (Remote)",
-        "desc": "During my time at Perasia as a Embedded Software Intern, I spearheaded the Arrhythmia ECG Classifier Project. Using ECG Data from the MIT-BIH dataset, I denoised the pure time-domain signals using a bandpass filter and wavelet transform, performed heart rate variability analysis using derivate, squared, and moving integral filters, and trained an deep convoluted neural network model for the classification of different beat types. Following that, I worked on the backend to setup an AWS IoT instance to accept MQTT messages, and created a FastAPI to query data from the AWS S3 bucket and DynamoDb to send to the FrontEnd Developer."
+        "desc": "At Perasia, I worked as a Embedded Software Intern. I spearheaded the Arrhythmia ECG Classifier Project. Using ECG Data from the MIT-BIH dataset, I denoised the pure time-domain signals using a bandpass filter and wavelet transform, performed heart rate variability analysis using derivate, squared, and moving integral filters, and trained an deep convoluted neural network model for the classification of different beat types. Following that, I worked on the backend to setup an AWS IoT instance to accept MQTT messages, and created a FastAPI to query data from the AWS S3 bucket and DynamoDb to send to the FrontEnd Developer."
     },
     "Siemens Technologies and Services Pvt. Ltd.": {
         "startDate": "July 25, 2022",
@@ -27,20 +31,20 @@ const companyDesc = {
     "Develop For Good": {
         "startDate": "October 30, 2023",
         "endDate": "February 24, 2024",
-        "location": "Remote",
+        "location": "Seattle, WA (Remote)",
         "desc": "Under Develop For Good, I worked as a Data Engineer Volunteer at the non-profit XtraMath. I created ETL pipelines using AWS Glue (leveraging PySpark) to normalize the data schema, expediting access to pivotal strategic data for the company. I also helped build a new data warehouse on AWS Redshift that can regularly get updates from DynamoDB and enable better reporting for teachers to estimate their students' progress."
     },
     "Husky Satellite Lab": {
         "startDate": "Autumn 2023",
         "endDate": "Present",
-        "location": "Seattle, WA, USA (In-Person)",
+        "location": "Seattle, WA, USA",
         "desc": "As a Software Engineer at Husky Satellite Labs, I worked on the PHAT-3 team (Platform for High-Altitude Testing). I worked on a datalink system that supports packaging, encoding, and decoding data to the high altitude test satellite, and also ideated a protocol for satellite movement to support a mockup star tracking camera."
     },
     "University of Washington": {
         "startDate": "Autumn 2021",
         "endDate": "Winter 2023",
-        "location": "Seattle, WA, USA (In-Person)",
-        "desc": "During my time as a student in the University of Washington, I held 3 roles as a student worker and volunteer. As a volunteer, I was a FIG leaader in Au '22 with a class size of about 24 students. A FIG leader holds weekly seminars to help freshmen navigate through their first-year at college so that they can find their path to success during their four years. As a student worker, I worked as a dining student assistant for Housing and Food Services (HFS) in Au '21 and a classroom technician for Academic Technologies in Wi '23."
+        "location": "Seattle, WA, USA",
+        "desc": "At the University of Washington, I held 3 roles as a student worker and volunteer. As a volunteer, I was a FIG leaader in Au '22 with a class size of about 24 students. A FIG leader holds weekly seminars to help freshmen navigate through their first-year at college so that they can find their path to success during their four years. As a student worker, I worked as a dining student assistant for Housing and Food Services (HFS) in Au '21 and a classroom technician for Academic Technologies in Wi '23."
     }
 }
 
@@ -78,7 +82,8 @@ export default function Experience() {
         <section id="experience-section">
             <h2>Experiences</h2>
             <div id="experiences">
-                <ExperienceBlock companyName={"Dozee"}/>
+                <ExperienceBlock companyName={"Dartmouth College"}/>
+                <ExperienceBlock companyName={"Dozee Health"}/>
                 <ExperienceBlock companyName={"Develop For Good"}/>
                 <ExperienceBlock companyName={"Perasia Technologies LLC"}/>
                 <ExperienceBlock companyName={"Siemens Technologies and Services Pvt. Ltd."}/>
